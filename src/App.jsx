ed73cf454b9d283ba506e362a33ce4d4a589be8e
kutluduman <kutluduman@gmail.com>
2021-05-17 22:46:26 -0400

    Installed styled component and created a sample button

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import PrimaryButton from './components/Buttons';
+
+
+function App() {
+  return (
+    <div>
+    <PrimaryButton>Hello World!</PrimaryButton>
+    </div>
+  );
+}
+
+export default App;
